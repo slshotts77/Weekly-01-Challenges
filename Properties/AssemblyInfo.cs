6353c665853a6327d8a3adc718d994f064ed4997
Steve <slshotts77@gmail.com>
2020-11-17 20:56:44 -0500

    Week 01 Challenge Update

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week_01_Challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week_01_Challenge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ebdb497-7040-4b6d-8176-80cb11181239")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week_01_Challenge.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week_01_Challenge
+{
+    [TestClass]
+    public class Week_01_Challenge
+    {
+        [TestMethod]
+        public void decalreAndInitializeVariables()
+        {
+            string firstName = "Steve";
+            string lastName = "Shotts";
+            int numGiven = 7;
+            int age = 43;
+
+            Console.WriteLine(age + numGiven);
+            Console.WriteLine(age - numGiven);
+            Console.WriteLine(age * numGiven);
+            Console.WriteLine(age / numGiven);
+            Console.WriteLine(age % numGiven);
+        }
+
+
+        [TestMethod]
+        public void decalreAndInitializeArray()
+        {
+            string[] books = { "Nelson DeMille, Plum Island ", "Nelson DeMille, Night Fall", "Nelson DeMille,THe Lion's Game", "John Grisham, Sycamore Row" };
+            foreach (string i in books)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        [TestMethod]
+        public void listOfDatesAndCurrent()
+        {
+            List<string> listOfDates = new List<string>();  //List<DateTime> listofDates = new List<DateTime>();
+            listOfDates.Add("11/01/2020");  //using example above this would be listOfDates.Add(new DateTime(2020/12/25));
+            listOfDates.Add("11/02/2020");
+            listOfDates.Add("11/03/2020");
+            listOfDates.Add("11/04/2020");
+            listOfDates.Add("11/05/2020");
+            listOfDates.Add("11/06/2020");
+            listOfDates.Add("11/07/2020");
+            listOfDates.Add("11/08/2020");
+            listOfDates.Add("11/09/2020");
+            listOfDates.Add("11/10/2020");
+
+
+            DateTime today = DateTime.Today;
+            //DateTime today = DateTime.Today;
+            //listOfDates.Add(today);
+        }
+
+
+        [TestMethod]
+        public void HowManyHoursofSleep()
+        {
+            Console.WriteLine("How many hours of sleep did you get?????");
+            int hours = 4;
+            if (hours >= 10)
+            {
+                Console.WriteLine("Wow, that's alot of sleep");
+            }
+            else if (hours > 8 && hours < 10)
+            {
+                Console.WriteLine("Your ahould be pretty ressted");
+            }
+            else if (hours > 4 && hours < 8)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else
+            {
+                Console.WriteLine("Oh man get some sleep");
+            }
+        }
+
+        [TestMethod]
+        public void HowHasYourDayBeen()
+        {
+            string userDay = "Okay";
+            switch (userDay)
+            {
+                case "Great":
+                    Console.WriteLine("Glad you are having a greatday");
+                    break;
+                case "Good":
+                    Console.WriteLine("It is a day to be good on");
+                    break;
+                case "Okay":
+                    Console.WriteLine("Hopefully it will gget better from here");
+                    break;
+                case "Bad":
+                    Console.WriteLine("There is always tomorrow");
+                    break;
+                case ":(":
+                    Console.WriteLine("I guess this is us now");
+                    break;
+                default:
+                    Console.WriteLine("Please enter somethhing above");
+                    break;
+            }
+        }
+
+        [TestMethod]
+        public void Supercalifragilisticexpialidocious()
+        {
+            string name = "Supercalifragilisticexpialidocious";
+            foreach (char letter in name)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("not a i");
+                }
+            }
+        }
+    }
+}
